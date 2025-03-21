b99cf39691f00a465f43d0abacf32122bb18e31f
Matthis <matthisgastesoyer@gmail.com>
2025-03-21 10:54:03 +0100

    Renamed bdd var to avoid conflicts

--- traitements/chargement_info_perso.php
+++ traitements/chargement_info_perso.php
@@ -5,19 +5,19 @@ use DBConfig\Database;
 function dbConnect(): PDO {
     return Database::getConnection();
 }
-$connexion = dbConnect();
+$mabdd = dbConnect();
 
 // Vérifiez la connexion
-if ($connexion->connect_error) {
-    die("Erreur de connexion : " . $connexion->connect_error);  
+if ($mabdd->connect_error) {
+    die("Erreur de connexion : " . $mabdd->connect_error);
 }
 // Préparez la requête SQL en utilisant des requêtes préparées pour des raisons de sécurité
 $requete = 'SELECT * FROM UTILISATEUR WHERE UTILISATEUR.Mail_Uti=?';
-$stmt = $connexion->prepare($requete);
+$stmt = $mabdd->prepare($requete);
 $stmt->bind_param("s", $_SESSION['Mail_Uti']); // "s" indique que la valeur est une chaîne de caractères
 $stmt->execute();
 $result = $stmt->get_result();
 
 $stmt->close();
-$connexion->close();
+$mabdd->close();
 ?>
